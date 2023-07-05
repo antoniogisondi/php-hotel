9b06acb251a6f101d79f6fb01462fa4164e8cae4
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-05 10:45:40 +0200

    Bonus 1 and 2 done and continue bonus 3

--- bonus/index.php
+++ bonus/index.php
@@ -1,5 +1,34 @@
 <?php 
-    include __DIR__.'/partials/vars.php'
+    include __DIR__.'/partials/vars.php';
+
+
+
+    if(isset($_GET['parking'])){
+        $park = $_GET['parking'];
+        $array_hotels = [];
+        foreach ($hotels as $index => $hotel){
+            if($hotel['parking'] == filter_var($park, FILTER_VALIDATE_BOOLEAN) ){
+                $array_hotels [] = $hotel;
+
+            }
+        };
+        $hotels = $array_hotels;
+    };
+
+
+    if(isset($_GET['vote']) && $_GET['vote'] !== ''){
+
+        $vote = $_GET['vote'];
+        $rating_hotels = [];
+
+        foreach($filter_hotels as $index => $hotel){
+            if($hotel['vote'] === $vote){
+                $rating_hotels [] = $hotels;
+            }
+        }
+
+        $hotels = $rating_hotels;
+    }
 ?>
 
 
@@ -13,7 +42,27 @@
     </head>
     <body>
         <div class="container-fluid">
-            <div class="row">
+            <div class="row d-flex flex-column">
+                <div class="col-12">
+                    <form action="index.php" method="GET">
+                        <div class="col">
+                            <label for="parking" class="control-label">Parcheggio</label>
+                            <select name="parking" id="parking">
+                                <option value="">Tutti</option>
+                                <option value="0">No</option>
+                                <option value="1">Si</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label for="vote" class="control-label">Voto</label>
+                            <input type="number" name="vote" id="vote">
+                        </div>
+                        <div class="col-auto">
+                            <button type="submit" class="btn btn-success">Cerca</button>
+                            <button type="reset" class="btn btn-warning">Reset</button>
+                        </div>
+                    </form>
+                </div>
                 <div class="col">
                     <div class="title">
                         <h2>Hotel disponibili</h2>
@@ -35,13 +84,7 @@
                             <td><?php echo $hotel['description'] ?></td>
                             <td><?php echo $hotel['vote']. " / ".'5' ?></td>
                             <td><?php echo $hotel['distance_to_center'].' Km '?></td>
-                            <td><?php if($hotel['parking'] == true){
-                                            echo $string_true = json_encode($hotel['parking']);
-                                            }
-                                            elseif($hotel['parking'] == false) {
-                                                echo $string_false = json_encode($hotel['parking']);
-                                            }
-                            ?></td>
+                            <td><?php echo ($hotel['parking'] == true) ? 'Si' : 'No'?></td>
                             </tr>
                             <?php } ?>
                         </tbody>
